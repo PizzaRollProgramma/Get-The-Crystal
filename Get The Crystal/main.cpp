04a013924316fea31f1d2c75824e53f3c1bbfb69
Ty Ternus <tyternus@tys-macbook-pro.attlocal.net>
2017-01-06 21:03:15 -0600

    Initial Commit

---
+++ Get The Crystal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Get The Crystal
+//
+//  Created by Ty Ternus on 1/6/17.
+//  Copyright © 2017 Ty Ternus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
